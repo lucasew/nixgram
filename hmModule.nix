1b2c48fae75f9a2dbebff1930d3b8c34a74a4580
lucasew <lucas59356@gmail.com>
2020-11-16 09:15:52 -0300

    add home-manager module
    Signed-off-by: lucasew <lucas59356@gmail.com>

---
+++ hmModule.nix
@@ -0,0 +1,55 @@
+{pkgs, lib, config, ...}@args:
+with lib;
+let 
+  nixgram = pkgs.callPackage ./package.nix {};
+  dotenv = import "${builtins.fetchGit {
+    url = "https://github.com/lucasew/dotenv";
+  }}";
+  nixgramService = dotenvFile: pkgs.writeShellScript "nixgram-service" ''
+    ${dotenv}/bin/dotenv @${builtins.toString dotenvFile} -- ${nixgram}/bin/nixgram
+  '';
+in
+{
+  options = {
+    services.nixgram = {
+      enable = mkEnableOption "enable telegram interface for custom commands";
+      dotenvFile = mkOption {
+        type = with types; path;
+        default = "/dev/null";
+        example = "/some/file/with/the/credentials.env";
+        description = "Dotenv file to load before starting nixgram";
+      };
+      customCommands = mkOption {
+        type = with types; attrsOf str;
+        default = {};
+        example = {
+          echo = "echo $*";
+        };
+        description = "Custom extra commands that the bot can handle";
+      };
+    };
+  };
+  config = mkIf config.services.nixgram.enable {
+    systemd.user.services.nixgram = {
+      Unit = {
+        Description = "Command bot for telegram";
+      };
+      Service = {
+        Type = "exec";
+        ExecStart = "${nixgramService config.services.nixgram.dotenvFile}";
+        Restart = "on-failure";
+      };
+      Install = {
+        WantedBy = ["default.target"];
+      };
+    };
+    home.packages = [
+      nixgram
+    ]
+    ++ builtins.attrValues (
+      builtins.mapAttrs 
+        (name: value: pkgs.writeShellScriptBin ("nixgram-" + name) value)
+        config.services.nixgram.customCommands
+    );
+  };
+}

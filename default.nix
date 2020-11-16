5ef93060e8b7e4806049125d21b465d0b921fc10
lucasew <lucas59356@gmail.com>
2020-11-16 09:06:31 -0300

    package in a separate file to avoid importing nixpkgs
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- default.nix
+++ default.nix
@@ -1,13 +1,3 @@
 let
   pkgs = import <nixpkgs> {};
-in pkgs.buildGoModule rec {
-  name = "nixgram";
-  version = "0.0.1";
-  vendorSha256 = "143fbg47yi3xlqkr328cgsa3gim3rkhzmv9l9x1s4v84l61r0s3j";
-  src = ./.;
-  meta = with pkgs.lib; {
-    description = "A smol software that can interface with commands using telegram";
-    homepage = "https://github.com/lucasew/nixgram";
-    platforms = platforms.linux;
-  };
-}
+in pkgs.callPackage ./package.nix {}
---
+++ package.nix
@@ -0,0 +1,12 @@
+{pkgs, ...}:
+pkgs.buildGoModule rec {
+  name = "nixgram";
+  version = "0.0.1";
+  vendorSha256 = "143fbg47yi3xlqkr328cgsa3gim3rkhzmv9l9x1s4v84l61r0s3j";
+  src = ./.;
+  meta = with pkgs.lib; {
+    description = "A smol software that can interface with commands using telegram";
+    homepage = "https://github.com/lucasew/nixgram";
+    platforms = platforms.linux;
+  };
+}
